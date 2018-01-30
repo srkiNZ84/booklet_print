feb4b969ba4f52d563de88f468336fe9ea950a22
Serge Dukic <serge.dukic@clearpoint.co.nz>
2018-01-30 21:50:25 +1300

    Committing naive approach for posterity
    So, I tried to do it by iterating over each of the pages, but this is
    the dumb way of doing it as you have to add complex logic to keep track
    of how far through you are.
    
    Better to work out how many sets of pages (booklets) there will be in
    total and then just iterate over each set.
    
    Issue(s): None

--- reorder.py
+++ reorder.py
@@ -7,7 +7,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Reorder a PDF file to be able \
         print four pages on a single piece of paper in "booklet" order.')
 parser.add_argument('inputpdf', type=str, help='PDF file to reorder.')
-parser.add_argument('-o', '--outputfile', metavar='-o', type=str, \
+parser.add_argument('-o', '--outputfile', type=str, \
         help='Name of the file to output the reordered manifest to. \
         Defaults to the "[ORIGINAL_FILENAME]-reodered.pdf"')
 args = parser.parse_args()
@@ -27,18 +27,43 @@ input1 = PdfFileReader(file(inputPDF, "rb"))
 # TODO: Need to specify how to print out to make a "booklet", right now have
 # to print "two pages per sheet".
 # TODO: Make it output two pages per sheet automatically
-output.addPage(input1.getPage(0).rotateCounterClockwise(180))
-output.addPage(input1.getPage(9).rotateCounterClockwise(180))
-output.addPage(input1.getPage(1))
-output.addPage(input1.getPage(8))
-output.addPage(input1.getPage(2).rotateCounterClockwise(180))
-output.addPage(input1.getPage(7).rotateCounterClockwise(180))
-output.addPage(input1.getPage(3))
-output.addPage(input1.getPage(6))
-output.addPage(input1.getPage(4).rotateCounterClockwise(180))
-output.addPage(input1.getPage(5).rotateCounterClockwise(180))
 
+# Get the number of pages
+print("document %s has %s pages." % (inputPDF, input1.getNumPages()))
+totalPages = input1.getNumPages()
 
+numBooklets = (totalPages + 9)/10
+
+print("Going to have %s booklets" % numBooklets)
+
+bookletCounter = 1
+for page in range(totalPages):
+
+    if(page % 2 == 0):
+        print("Getting page %s , flipping" % page)
+        output.addPage(input1.getPage(page).rotateCounterClockwise(180))
+        print("Getting page %s , flipping" % (9 - page))
+        output.addPage(input1.getPage(9 - page).rotateCounterClockwise(180))
+    else:
+        print("Getting page %s " % page)
+        output.addPage(input1.getPage(page))
+        print("Getting page %s " % (9 - page))
+        output.addPage(input1.getPage(9 - page))
+
+    if(page % 5 == 0):
+        bookletCounter += 1
+        print("Starting new booklet %s" % bookletCounter)
+
+#output.addPage(input1.getPage(0).rotateCounterClockwise(180))
+#output.addPage(input1.getPage(9).rotateCounterClockwise(180))
+#output.addPage(input1.getPage(1))
+#output.addPage(input1.getPage(8))
+#output.addPage(input1.getPage(2).rotateCounterClockwise(180))
+#output.addPage(input1.getPage(7).rotateCounterClockwise(180))
+#output.addPage(input1.getPage(3))
+#output.addPage(input1.getPage(6))
+#output.addPage(input1.getPage(4).rotateCounterClockwise(180))
+#output.addPage(input1.getPage(5).rotateCounterClockwise(180))
 
 # Write "output" to document-output.pdf
 print("Writing reordered page to PDF file '" + outputFile + "'...")
